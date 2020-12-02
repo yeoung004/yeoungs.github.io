725ae89b930cf860f2466fca73567a4d57705d15
yeoung004 <3930722@naver.com>
2020-12-02 11:37:07 +0900

    It was perfect game...:)

--- test/src/BowlingGame.java
+++ test/src/BowlingGame.java
@@ -8,6 +8,8 @@ public class BowlingGame {
 	final static int SECOND_BALL = 2;
 	final static int FINAL_BALL = 3;
 	final static int FULL_PIN = 10;
+	final static int FIRST_FRAME = 1;
+	final static int LAST_FRAME = 10;
 
 	public static void main(String[] args) {
 		Scanner roll = new Scanner(System.in);
@@ -24,6 +26,7 @@ public class BowlingGame {
 		int frame = 1;
 		int total = 0;
 		int nScore = 0;
+		boolean lastBall = false;
 
 		Map<String, Boolean> nowStatus = new HashMap<String, Boolean>();
 
@@ -34,7 +37,7 @@ public class BowlingGame {
 
 		System.out.println("-------------------프로그램을 시작합니다-------------------");
 
-		for (frame = 1; frame <= 9; frame++) {
+		for (frame = FIRST_FRAME; frame <= LAST_FRAME; frame++) {
 			if (score.equals("y"))
 				break;
 			for (ball = FIRST_BALL; ball <= SECOND_BALL; ball++) {
@@ -69,130 +72,105 @@ public class BowlingGame {
 				total += nScore;
 				printTemp = "출력 = " + score + "[" + frame + ", " + ball + ", ";
 
-				if (ball == FIRST_BALL) {
-					if (nowStatus.get("isSpare")) {
-						total += nScore;
-						nowStatus = updateStatus(nowStatus, "");
-					} else if (nowStatus.get("isStrike")) {
-						total += nScore;
-					} else if ((nowStatus.get("isTurkey") || nowStatus.get("isDouble"))) {
-						total += nScore * 2;
+				if (frame < 10) {
+					if (ball == FIRST_BALL) {
+						if (nowStatus.get("isSpare")) {
+							total += nScore;
+							nowStatus = updateStatus(nowStatus, "");
+						} else if (nowStatus.get("isStrike")) {
+							total += nScore;
+						} else if ((nowStatus.get("isTurkey") || nowStatus.get("isDouble"))) {
+							total += nScore * 2;
+						}
+					} else {
+						if (isOneOfStrikeTurkeyDouble(nowStatus)) {
+							total += nScore;
+							nowStatus = updateStatus(nowStatus, "");
+						}
 					}
-				} else {
-					if (isOneOfStrikeTurkeyDouble(nowStatus)) {
-						total += nScore;
-						nowStatus = updateStatus(nowStatus, "");
+
+					if (pin == EMPTY_PIN) {
+						if (ball == FIRST_BALL) {
+							if (nowStatus.get("isTurkey")) {
+								totalTemp = total + "+10+10+";
+								nowStatus = updateStatus(nowStatus, "isTurkey");
+							} else if (nowStatus.get("isDouble")) {
+								totalTemp += "10+";
+								nowStatus = updateStatus(nowStatus, "isTurkey");
+							} else if (nowStatus.get("isStrike")) {
+								totalTemp += "10+";
+								nowStatus = updateStatus(nowStatus, "isDouble");
+							} else {
+								totalTemp = total + "+";
+								nowStatus = updateStatus(nowStatus, "isStrike");
+							}
+							printTemp += totalTemp;
+						} else if (ball == SECOND_BALL) {
+							nowStatus = updateStatus(nowStatus, "isSpare");
+							printTemp += total + "+";
+						}
+					} else {
+						if (isOneOfStrikeTurkeyDouble(nowStatus) && ball == FIRST_BALL) {
+							printTemp += totalTemp + score + "+";
+						} else
+							printTemp += total;
 					}
+					
+				} else if (frame == LAST_FRAME) {
+					if (!lastBall) {
+						result[frame - 1][ball - 1] = nScore;
+
+						if (nowStatus.get("isSpare")) {
+							total += nScore;
+							nowStatus = updateStatus(nowStatus, "");
+						} else if (isOneOfStrikeTurkeyDouble(nowStatus)) {
+							if (ball == SECOND_BALL) {
+								total += nScore;
+								nowStatus = updateStatus(nowStatus, "");
+							} else {
+								if (nowStatus.get("isStrike"))
+									total += nScore;
+								else
+									total += nScore * 2;
+								if (nowStatus.get("isTurkey"))
+									totalTemp = total + "+10+" + score + "+";
+								else
+									totalTemp += score + "+";
+							}
+						}
+					}
+
+					if (isOneOfStrikeTurkeyDouble(nowStatus))
+						printTemp += totalTemp;
+					else
+						printTemp += total;
+
 				}
 
-				if (pin == EMPTY_PIN) {
-					if (ball == FIRST_BALL) {
-						if (nowStatus.get("isTurkey")) {
-							totalTemp = total + "+10+10+";
-							nowStatus = updateStatus(nowStatus, "isTurkey");
-						} else if (nowStatus.get("isDouble")) {
-							totalTemp += "10+";
-							nowStatus = updateStatus(nowStatus, "isTurkey");
-						} else if (nowStatus.get("isStrike")) {
-							totalTemp += "10+";
-							nowStatus = updateStatus(nowStatus, "isDouble");
-						} else {
-							totalTemp = total + "+";
-							nowStatus = updateStatus(nowStatus, "isStrike");
-						}
-						printTemp += totalTemp;
-					} else if (ball == SECOND_BALL) {
-						nowStatus = updateStatus(nowStatus, "isSpare");
-						printTemp += total + "+";
-					}
-				} else {
-					if (isOneOfStrikeTurkeyDouble(nowStatus) && ball == FIRST_BALL) {
-						printTemp += totalTemp + score + "+";
-					} else
-						printTemp += total;
-				}
+				if (lastBall)
+					printTemp = "출력 = " + score + "[10, 3, " + total;
 
 				nowScore(printTemp);
 
 				if (pin == EMPTY_PIN || ball == SECOND_BALL) {
 					pin = FULL_PIN;
-					break;
-				}
-			}
-		}
-		// 10번 프레임
-		if (!score.equals("y")) {
-			for (ball = FIRST_BALL; ball <= FINAL_BALL; ball++) {
-				if (pin == EMPTY_PIN)
-					pin = FULL_PIN;
-				if (score.equals("y"))
-					break;
 
-				// 몇 번째 투구
-				ballCnt++;
-
-				System.out.print("> " + ballCnt + " 번째 입력 값 ");
-				score = roll.nextLine();
-
-				if (score.equals("")) {
-					System.out.print("정말 종료하시겠습니까? y/n : ");
-					score = roll.nextLine();
-
-					if (score.equals("y")) {
+					if (frame != LAST_FRAME || lastBall)
 						break;
-					}
-					System.out.println("다시 시작합니다...");
-					ballCnt--;
-					frame--;
-					continue;
-				} else if (!isCorrect(score, pin)) {
-					ballCnt--;
-					ball--;
-					continue;
-				}
-
-				nScore = Integer.parseInt(score);
-				pin -= nScore;
-				total += nScore;
-				printTemp = "출력 = " + score + "[" + frame + ", " + ball + ", ";
-				
-				
-				if (ball != FINAL_BALL) {
-					result[frame - 1][ball - 1] = nScore;
-
-					if (nowStatus.get("isSpare")) {
-						total += nScore;
-						nowStatus = updateStatus(nowStatus, "");
-					} else if (isOneOfStrikeTurkeyDouble(nowStatus)) {
-						if (ball == SECOND_BALL) {
-							total += nScore;
-							nowStatus = updateStatus(nowStatus, "");
-						} else {
-							if (nowStatus.get("isStrike"))
-								total += nScore;
-							else
-								total += nScore * 2;
-							if (nowStatus.get("isTurkey"))
-								totalTemp = total + "+10+" + score + "+";
-							else
-								totalTemp += score + "+";
-						}
+					else if (ball != FIRST_BALL) {
+						if (result[9][0] + result[9][1] == FULL_PIN || result[9][0] == FULL_PIN) {
+							lastBall = true;
+							ball--;
+						} else
+							break;
 					}
 				}
 
-				if (isOneOfStrikeTurkeyDouble(nowStatus))
-					printTemp += totalTemp;
-				else
-					printTemp += total;
-
-				nowScore(printTemp);
-
-				if (result[9][0] != FULL_PIN && (ball == SECOND_BALL && pin != EMPTY_PIN))
-					break;
 			}
-			show(result, score);
-			System.out.println("최종점수 :" + total);
 		}
+
+		show(result, score);
+		System.out.println("최종점수 :" + total);
 
 		System.out.println("-------------------프로그램을 종료합니다-------------------");
 
@@ -261,22 +239,23 @@ public class BowlingGame {
 				System.out.print("\t|\t" + result[i][0] + ", " + result[i][1] + "");
 		}
 
-		if (result[9][0] + result[9][1] == 10)
-			System.out.print("\t|\t" + result[9][0] + ", /, ");
-		else if (result[9][0] + result[9][1] == 20)
+		if (result[9][0] + result[9][1] == 20)
 			System.out.print("\t|\tX, X, ");
+		else if (result[9][0] + result[9][1] == 10)
+			System.out.print("\t|\t" + result[9][0] + ", /, ");
 		else if (result[9][0] + result[9][1] > 10)
 			System.out.print("\t|\tX, " + result[9][1] + ", ");
 		else
-			System.out.print("\t|\t" + result[9][0] + ", " + result[9][1] + ",");
+			System.out.println("\t|\t" + result[9][0] + ", " + result[9][1] + ", 0");
 
-		// 3구 스트라이크
-		if (score.equals("10"))
-			System.out.println("X\t|");
-		else if ((result[9][0] + result[9][1]) != 10 && (Integer.parseInt(score) + result[9][1]) == 10)
-			System.out.println("/\t|");
-		else
-			System.out.println(score + "\t|");
+		if (result[9][0] + result[9][1] >= 10) {
+			if (score.equals("10"))
+				System.out.println("X\t|");
+			else if ((result[9][0] + result[9][1]) != 10 && (Integer.parseInt(score) + result[9][1]) == 10)
+				System.out.println("/\t|");
+			else
+				System.out.println(score + "\t|");
+		}
 
 		System.out.println(
 				"##################################################기록판###############################################");
